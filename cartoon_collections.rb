bd4cbb6d4906b141c41c5d84164399a4c3512af9
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-06-27 20:51:00 -0400

    commit tree 738f8c34709f866dedb3d34bd7aa5aab68e0c3da

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+
+def roll_call_dwarves(names)# code an argument here
+  names.each.with_index do |name, index| # Your code here
+    puts "#{index+1}.*#{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)# code an argument here
+  return calls.map do |call|# Your code here
+            call.capitalize() << "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def long_planeteer_calls(calls)# code an argument here
+    return calls.any? do |i|# Your code here
+      i.length > 4
+    end
+    
+  end
+
+def find_the_cheese(strings)# code an argument here
+    cheese_types = ["cheddar", "gouda", "camembert"]
+    return strings.detect do |string|# the array below is here to help
+      string == "cheddar"
+    end
+    
 end
